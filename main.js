922fb2792c55895a6c8ecdbc97ec80ed21de4623
alaa mohammed <3laamohamed12@gmail.com>
2023-07-03 06:11:19 +0200

    n

---
+++ main.js
@@ -0,0 +1,69 @@
+// window.alert("be carful");
+// document.write("<h1>hello from file </h2>");
+// console.log("hello from console")
+
+// console.log("find element")
+// console.error("error")
+// console.log("%cfind %celement" , "color:red; font-size:40px" , "color:blue; font-size:40px")
+
+// var my_name = "osama"
+
+// console.log("my name is " + my_name);
+
+// console.log("alaa mohammed")
+// console.log(typeof 2000)
+// var name = "alaa mohammed";
+// console.log(name)
+
+// var name = "alaa";
+// let age = 23;
+// const country = "eg";
+
+// console.log("alaa mohamed is \"student\"");
+// console.log("Alaa\nmohamed");
+
+// let name = "alaa is ";
+// let age = 23;
+// document.write(name + age )
+// let name = "alaa"
+// let markup = `<div>
+//     <h2>title</h2>
+//     <p>${name}</p>
+// </div>`
+
+// document.write(markup)
+
+// document.write(10 + 10);
+// document.write(10 - 10);
+// document.write(10 * 10);
+// document.write(10 % 10);
+// document.write(10 / 10);
+// document.write(10 ** 10);
+
+// console.log(+"100")
+// console.log(-"100")
+
+// console.log(+100)
+// console.log(-100)
+
+// console.log(Number("100"))
+
+// console.log(100..toString())
+// console.log(100.100.toFixed(2))
+// console.log(parseInt("1000"))
+// console.log(parseFloat("1000"))
+// console.log(Math.round(10.11))
+// console.log(Math.min(10.11))
+// console.log(Math.max(10.11))
+// console.log(Math.ceil(10.11))
+// console.log(Math.floor(10.11))
+// console.log(Math.random())
+// console.log(Math.trunc(10.11))
+
+// let my_name = "alaa mohamed "
+// console.log(my_name.charAt(2));
+// console.log(my_name.toUpperCase());
+// console.log(my_name.length);
+// console.log(my_name.trim());
+// console.log(my_name[2]);
+
